2259e22475e930b444cfdc16d2531c8edaaf2820
nurfiiitriiiyah <nurfiitriiyah@gmail.com>
2019-05-08 15:58:54 +0700

    instagram clone

--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import VueSweetalert2 from 'vue-sweetalert2';
+// // The Vue build version to load with the `import` command
+// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+// import axios from 'axios';
+// import VueAxios from 'vue-axios';
+// import VueSweetalert2 from 'vue-sweetalert2';
+//
+// Vue.use(VueSweetalert2, options)
+// Vue.use(VueAxios, axios);
+//
+// Vue.config.productionTip = false
+//
+// const options = {
+//   confirmButtonColor: '#68b3ab',
+//   cancelButtonColor: '#ff7674'
+// }
+//
+//
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
 
-Vue.use(VueSweetalert2, options)
-Vue.use(VueAxios, axios);
+import Vue from "vue";
+import App from "./App";
 
-Vue.config.productionTip = false
-
-const options = {
-  confirmButtonColor: '#68b3ab',
-  cancelButtonColor: '#ff7674'
-}
-
-
+/* eslint-disable no-new */
 new Vue({
-  router,
+  el: "#app",
   render: h => h(App)
-}).$mount('#app')
+});
